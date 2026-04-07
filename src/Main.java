9781a70ffc578507a31b8ec243728e50cd8ff17a
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-04-07 12:28:04 +0700

    Make some test

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
 void main() {
-
+    int a = 1, b = 2;
+    System.out.println(sumCal(a,b));
 }
+
+static int sumCal(int... numbers) {
+    int sum = 0;
+    for (int i : numbers) {
+        sum += i;
+    }
+    return sum;
+}
+
+
